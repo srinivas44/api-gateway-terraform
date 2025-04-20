54440ee87661f085c504246e5a32cbfe08f29020
Srinivas <Srinivas@localhost.com>
2025-04-20 20:44:33 +0100

    Modified again and again outputs

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,7 @@ output "api_gateway_id" {
   description = "ID of the created API Gateway"
   value       = module.api_gateway.api_gateway_id
 }
-# Naks line ##
+# Naks lin ##
 ##Added new line
+
+###Added new line3
